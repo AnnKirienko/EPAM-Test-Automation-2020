7e44890ba74ab5eaae2ad9d649d3efef840c5078
Anna Kirienko <anna.kirienko.st@gmail.com>
2019-09-25 23:00:50 +0300

    Add my first program version 2

---
+++ TaskSh1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace TaskSh1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input your string:");
+            string str = Console.ReadLine();
+
+            // exeption if our string is empty
+            if ( str.Length == 0 )
+            {
+                System.Environment.Exit(1);
+            }
+
+            int count = 1;
+            int maxLength = 1;
+
+            // create new variable for storage previous letter
+            char previousLetter = str[0]; 
+
+            for ( int i = 1; i < str.Length; i++ )
+            {
+                if ( previousLetter == str[i] )
+                {
+                    count++;
+                }
+
+                else
+                {
+                    previousLetter = str[i];
+                    count = 1;
+                }
+
+                if ( maxLength < count )
+                {
+                    maxLength = count;
+                }    
+            }
+
+            Console.Write( maxLength );
+        }
+    }
+}
