1db8a1e084e6362536f62c26dd50e25b920caebe
Anna Kirienko <anna.kirienko.st@gmail.com>
2019-11-13 03:09:47 +0300

    Add homework with all tasks

---
+++ Chess/Cell.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+   public class Cell
+    {
+
+        char column;
+        int row;
+        const int sizeOfBoard = 8;
+
+        public Cell(char column, int row)
+        {
+            if ((column >= 'A' && column <= 'H') && (row <= 8 && row >= 1))
+            {
+                this.column = column;
+                this.row = row;
+            }
+            else
+                throw new MyNotCorrectedInputException("Input your cell again:\n");
+        }
+
+        private int GetNumberIndexOfLetter()
+        {
+            return (int)column - 65 + 1;
+        }
+
+        public string GetCellColour()
+        {
+            if ((this.GetNumberIndexOfLetter() + this.row)%2 == 0 )
+            {
+                return "Black";
+            }
+            else
+            {
+                return "White";
+            }
+        }
+
+        public bool CanBeInOneVerticalLine(Cell cell)
+        {
+            return this.column == cell.column;
+        }
+
+        public bool CanBeInOneGorizontalLine(Cell cell)
+        {
+            return this.row == cell.row;
+        }
+
+        public bool CanBeInOneDiagonalLine(Cell cell)
+        {
+            return (Math.Abs(this.row - this.GetNumberIndexOfLetter())) == (Math.Abs(cell.row - cell.GetNumberIndexOfLetter() ));
+        }
+
+
+    }
+}
---
+++ Chess/MyNotCorrectedException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    class MyNotCorrectedInputException : Exception
+    {
+       
+     public MyNotCorrectedInputException(string message) : base(message)
+            {
+            }
+            public MyNotCorrectedInputException()
+            { }
+        
+
+    }
+}
