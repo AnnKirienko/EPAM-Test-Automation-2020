b3b75ce23ba0e4eb3dd4a1350ba64829ec7737f9
Anna Kirienko <anna.kirienko.st@gmail.com>
2019-11-21 02:17:53 +0300

    add solution without tests

---
+++ DEV-3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DEV_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Transliterator transliterator = new Transliterator();
+        }
+    }
+}
