81aedb3645a9d66de4ec965fc79ad7ef383c452f
AnnKirienko <anna.kirienko.st@gmail.com>
2020-02-09 03:12:47 +0300

    Add task

---
+++ Task_1.2/NumeralSystemConverter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+
+namespace Task_1_2
+{
+    public class NumeralSystemConverter
+    {
+        private Dictionary<int, string> numLetMapping = new Dictionary<int, string>(11)
+        {
+            { 10,"A"}, { 11,"B"},{12,"C" },{13, "D"},{14,"E"},{ 15,"F"},{16,"G"},{17,"H"},{ 18,"I"},{19,"J"},{20,"K"}
+        };
+
+        public string Convert(int decimalNumber, int systemBase)
+        {
+            
+
+            int divisionRemainder; 
+            int divisionQuotient; 
+
+            StringBuilder resultNumber = new StringBuilder();
+
+           
+            while (true)
+            {
+                divisionQuotient = decimalNumber / systemBase;
+                divisionRemainder = decimalNumber % systemBase;
+                decimalNumber = divisionQuotient;
+
+                resultNumber.Insert(0, divisionRemainder > 9 ? ConvertNumberToLetter(divisionRemainder, numLetMapping) : divisionRemainder.ToString());
+
+                if (divisionQuotient < 1)
+                {
+                    break;
+                }
+            }
+
+            return resultNumber.ToString();
+        }
+
+        private string ConvertNumberToLetter(int number, Dictionary<int, string> mapping)
+        {
+            return mapping[number];
+        }
+    }
+}
---
+++ Task_1.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input your decimal number and base of a new number system: ");
+            int decimalNumber;
+            while (!int.TryParse(Console.ReadLine(), out decimalNumber)&& decimalNumber < 0)
+            {
+                Console.Write("Format error! Input decimal number again: ");
+            }
+            
+            int baseOfANewNumberSystem;
+            while (!int.TryParse(Console.ReadLine(), out baseOfANewNumberSystem) && baseOfANewNumberSystem < 2 && baseOfANewNumberSystem > 20)
+            {
+                Console.Write("Format error! Input a base of a new number system again: ");
+            }
+            
+            NumeralSystemConverter numberSystemConverter = new NumeralSystemConverter();
+
+            Console.WriteLine(numberSystemConverter.Convert(decimalNumber, baseOfANewNumberSystem));
+        }
+    }
+}
